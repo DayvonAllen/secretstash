4bdc5980e358cfbfa95ed1adda03e54a5bf0d65e
DayvonAllen <dayvonallen93@outlook.com>
2020-10-31 07:06:17 -0700

    first commit

--- backend/src/main/java/com/dna/dev/secretstash/SecretStashApplication.java
+++ backend/src/main/java/com/dna/dev/secretstash/SecretStashApplication.java
@@ -1,13 +1,13 @@
-package com.dna.dev.demo;
+package com.dna.dev.secretstash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SecretStashApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SecretStashApplication.class, args);
     }
 
 }
